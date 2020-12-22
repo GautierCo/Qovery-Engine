80380d2515a0001696ea45b6d72a0e9d8b9a88df
GautierCo <gautier.colasse@gmail.com>
2020-12-23 00:13:09 +0100

    Integration completed

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,19 @@
-console.log("App started");
+
+const animateWorkflow = (workflow, action) => {
+    const workflowItems = workflow.target.querySelector("#workflow1").querySelectorAll(".work");
+    workflowItems.forEach((element) => {
+        if(action === "remove") element.classList.remove("workflow-item");
+        if(action === "add") element.classList.add("workflow-item");
+    });
+};
+
+const intersectionObserver = new IntersectionObserver((entries) => {
+    const workflow = entries[0];
+    if (workflow.intersectionRatio <= 0) {
+        //animateWorkflow(workflow, "remove")
+    } else {
+        animateWorkflow(workflow, "add");
+    }
+});
+
+intersectionObserver.observe(document.querySelector(".workflow"));
